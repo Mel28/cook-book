1c62a70f5dba17db390bf9e094c4b91c7f44f607
Melanie Milsted <melanie.milsted@gmail.com>
2019-02-10 12:23:58 +0000

    added accordion feature and conected application to materialize

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
-
-app.config["MONGO_DBNAME"] = 'cook-book'
+app.config["MONGO_DBNAME"] ='cook-book'
 app.config["MONGO_URI"] ='mongodb://Mel28:AlexGauci333@ds131601.mlab.com:31601/cook-book'
 
 mongo = PyMongo(app)
@@ -17,11 +16,7 @@ def get_recipes():
     return render_template("recipes.html", 
         tasks=mongo.db.recipes.find())
 
-@app.route('/')
-def hello():
-    return 'Hello world....third time lucky!'
-    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+            port=int(os.environ.get('PORT')),
+            debug=True)
