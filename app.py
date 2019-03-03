85ea03bc7a5029d0f289b1415dfc11e0b905c231
Melanie Milsted <melanie.milsted@gmail.com>
2019-03-03 11:13:32 +0000

    Added to addrecipe.html and created editrecipe.html. Added functionality to add recipe and edit recipe pages in app.py. Added Nav bar to base.html, needs fixing still.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import  Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
@@ -13,8 +13,50 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", 
-        tasks=mongo.db.recipes.find())
+    return render_template('recipes.html', 
+        recipes=mongo.db.recipes.find())
+        
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html',
+        recipes = mongo.db.recipes.find())
+    
+    
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes =  mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
+
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name':request.form.get['recipe_name'],
+        'category_name':request.form.get['category_name'],
+        'recipe_ingredients': request.form.get['recipe_ingredients'],
+        'recipe_method': request.form.get['recipe_method'],
+        'contains_meat':request.form.get['contains_meat'],
+        'country_origin':request.form.get['country_origin']
+        
+    })
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
