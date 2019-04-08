2b86329904ff9110bea0ce4a19b1ff905429a3e9
Melanie Milsted <melanie.milsted@gmail.com>
2019-04-08 12:21:19 +0000

    updated byotest.py, tested code, All tests pass!

--- testing/byotest.py
+++ testing/byotest.py
@@ -1,14 +1,14 @@
-def number_of_evens(numbers):
-    return 0
-def test_are_equal(actual, expected):
-    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+from byotest import*
+import os
 
-
-def test_not_equal(a, b):
-    assert a != b, "Did not expect {0}, but got {1}".format(a, b)
-
-
-def test_is_in(collection, item):
-    assert item in collection, "{0} does not contain {1}".format(collection, item)
-    
-test_are_equal(number_of_evens([1, 2, 3, 4, 5]), 2)
+def test_index(get, self):
+        """Route Testing"""
+        response = get('/index', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        
+def test_scoreboard(self, get, response):
+        """Route Testing"""
+        response = get('/scoreboard', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        
+print("All tests pass!")
